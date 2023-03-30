9b388ce9cde328b3e62a705ebe9623f4208839c1
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2023-03-30 13:58:57 +0300

    Создана страница входа

---
+++ testwork-for-1T/src/pages/Authorization/EnterPage.vue
@@ -0,0 +1,47 @@
+<template>
+    <q-page class="q-pa-md">
+        <h5 class="row">Добро пожаловать! Войдите чтобы продолжить</h5>
+        <div class="flex flex-center">
+            <q-form @submit="EnterSubmit"
+                    @reset="EnterReset">
+                <q-input rounded
+                        v-model="email"
+                        label="Введите вашу почту"
+                        type="email">
+                </q-input>
+                <q-input rounded
+                        v-model="password"
+                        label="Введите ваш пароль"
+                        type="password">
+                </q-input>
+                <div class="q-mt-md">
+                    <q-btn label="Отправить" type="submit" color="primary"/>
+                    <q-btn label="Сбросить" type="reset" color="primary" flat class="q-ml-sm" />
+                </div>
+                <p class="q-pt-md">Еще нет аккаунта? <router-link to="/register">зарегистрируйте его сейчас</router-link></p>
+                <p><router-link to="/reset">Восстановить пароль</router-link></p>
+            </q-form>
+        </div>
+    </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+    setup() {
+        const email = ref("")
+        const password = ref("")
+        return{
+            EnterSubmit(){
+                console.log("submitted");
+            },
+            EnterReset(){
+                console.log("reseted");
+            },
+            email,
+            password
+        }
+    },
+})
+</script>
--- testwork-for-1T/src/router/routes.js
+++ testwork-for-1T/src/router/routes.js
@@ -2,11 +2,12 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('layouts/AuthorizationLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/Authorization/EnterPage.vue') }
     ]
   },
+
 
   // Always leave this as last one,
   // but you can also remove it
