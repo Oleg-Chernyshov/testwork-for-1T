7e03d8ec19c944185e0e51b3e2d4f579285d4e42
Gelato <you@example.com>
2023-04-01 23:37:10 +0300

    Сделал на странице Team возможность добавлять пользователя в определенную группу по почте

--- testwork-for-1T/src/pages/Team.vue
+++ testwork-for-1T/src/pages/Team.vue
@@ -3,19 +3,25 @@
     Team
     <div class="wrapper join">
       <div class="join__block">
-        <form class="join__form-executer" action="">
-          <input type="text" placeholder="Имя" />
-          <input type="text" placeholder="Фамилия" />
-          <input type="text" placeholder="Почта" />
+        <form
+          @submit.prevent="getFormExecuterValues"
+          class="join__form-executer"
+        >
+          <input name="name" type="text" placeholder="Имя" />
+          <input name="surname" type="text" placeholder="Фамилия" />
+          <input name="eMail" type="text" placeholder="Почта" />
           <button>Добавить в группу Исполнители</button>
         </form>
       </div>
 
       <div class="join__block">
-        <form class="join__form-executer" action="">
-          <input type="text" placeholder="Имя" />
-          <input type="text" placeholder="Фамилия" />
-          <input type="text" placeholder="Почта" />
+        <form
+          @submit.prevent="getFormResponsibleValues"
+          class="join__form-executer"
+        >
+          <input name="name" type="text" placeholder="Имя" />
+          <input name="surname" type="text" placeholder="Фамилия" />
+          <input name="eMail" type="text" placeholder="Почта" />
           <button>Добавить в группу Ответственные</button>
         </form>
       </div>
@@ -28,11 +34,19 @@ import { useMutation } from "@vue/apollo-composable";
 import { defineComponent, ref, computed } from "vue";
 import { useQuery } from "@vue/apollo-composable";
 import gql from "graphql-tag";
+import { getClientOptions } from "src/apollo/index";
+import { provideApolloClient } from "@vue/apollo-composable";
+import { ApolloClient } from "@apollo/client/core";
+import { useQuasar } from "quasar";
+
 export default {
   components: {},
 
   setup() {
-    {
+    const $q = useQuasar();
+    const getFormExecuterValues = function (e, n) {
+      const apolloClient = new ApolloClient(getClientOptions());
+      provideApolloClient(apolloClient);
       const { mutate } = useMutation(
         gql`
           mutation userGroupInviteUser($input: UserGroupInviteUserInput!) {
@@ -44,19 +58,87 @@ export default {
         () => ({
           variables: {
             input: {
-              name: "Денис",
-              surname: "Королев",
-              email: "dkorolyov631@mail.ru",
+              name: e.target.elements.name.value,
+              surname: e.target.elements.surname.value,
+              email: e.target.elements.eMail.value,
               page_group_id: "2289312306467874395",
             },
           },
         })
       );
       const response = mutate();
-      response.then(function (result) {});
-    }
+      response
+        .then(function (result) {
+          console.log("getFormExecuterValues", result);
+          $q.notify({
+            type: "positive",
+            message: "Отправлено",
+          });
+        })
+        .catch((err) => {
+          console.log("Ошибка", err);
+          $q.notify({
+            type: "negative",
+            message: "Ошибка отправки",
+          });
+        });
+      [
+        e.target.elements.name.value,
+        e.target.elements.surname.value,
+        e.target.elements.eMail.value,
+      ] = ["", "", ""];
+      console.log(n);
+    };
 
-    return {};
+    const getFormResponsibleValues = function (e) {
+      const apolloClient = new ApolloClient(getClientOptions());
+      provideApolloClient(apolloClient);
+      const { mutate } = useMutation(
+        gql`
+          mutation userGroupInviteUser($input: UserGroupInviteUserInput!) {
+            userGroupInviteUser(input: $input) {
+              status
+            }
+          }
+        `,
+        () => ({
+          variables: {
+            input: {
+              name: e.target.elements.name.value,
+              surname: e.target.elements.surname.value,
+              email: e.target.elements.eMail.value,
+              page_group_id: "3532947332290869241",
+            },
+          },
+        })
+      );
+      const response = mutate();
+      response
+        .then(function (result) {
+          console.log("getFormExecuterValues", result);
+          $q.notify({
+            type: "positive",
+            message: "Отправлено",
+          });
+        })
+        .catch((err) => {
+          console.log("Ошибка", err);
+          $q.notify({
+            type: "negative",
+            message: "Ошибка отправки",
+          });
+        });
+      [
+        e.target.elements.name.value,
+        e.target.elements.surname.value,
+        e.target.elements.eMail.value,
+      ] = ["", "", ""];
+    };
+
+    return {
+      getFormExecuterValues,
+      getFormResponsibleValues,
+    };
   },
 };
 </script>
