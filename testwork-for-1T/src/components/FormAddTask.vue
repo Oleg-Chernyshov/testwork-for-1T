7000a4eee43f4d8bb878f754c2139af15724bee5
Gelato <you@example.com>
2023-04-12 02:08:57 +0300

    настройка fetch

--- testwork-for-1T/src/components/FormAddTask.vue
+++ testwork-for-1T/src/components/FormAddTask.vue
@@ -81,7 +81,6 @@ export default defineComponent({
     const options = computed(() => store.getters.OPTIONS_EXECUTORS);
     const statusId = ref("");
     const refetchModules = store.getters.REFETCH_MODULES;
-    console.log("ref1", refetchModules);
 
     watch(model, () => {
       indexExecutor.value = options.value.indexOf(model.value);
@@ -118,7 +117,10 @@ export default defineComponent({
           },
         },
       }));
+      console.log("refetchModules", refetchModules);
       response("Задача добавена", "Ошибка", mutate, refetchModules, $q);
+      e.target.elements.name.value = "";
+      e.target.elements.description.value = "";
     };
     return {
       options,
