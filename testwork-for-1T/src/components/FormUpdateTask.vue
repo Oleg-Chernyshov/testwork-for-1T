4ba0e7ff26e88b689dd40663b64beb1ac919cf69
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2023-04-13 18:51:21 +0300

    Обновление доступов для задач

--- testwork-for-1T/src/components/FormUpdateTask.vue
+++ testwork-for-1T/src/components/FormUpdateTask.vue
@@ -46,7 +46,9 @@
 import { useMutation } from "@vue/apollo-composable";
 import { defineComponent, ref, computed, watch } from "vue";
 import { getClientOptions } from "src/apollo/index";
-import { updateUser, createRule } from "../api/main/mutations";
+import { useQuery } from "@vue/apollo-composable";
+import { updateUser, createRule, permissionRuleDelete } from "../api/main/mutations";
+import { permissionTreeSubjects } from "src/api/main/queryes";
 import { provideApolloClient } from "@vue/apollo-composable";
 import { ApolloClient } from "@apollo/client/core";
 import { useQuasar } from "quasar";
@@ -134,26 +136,46 @@ export default defineComponent({
       const response = mutate();
       response
         .then(function (result) {
-          console.log(result);
-          const { mutate } = useMutation(createRule, ()=>({
+          const { onResult } = useQuery(permissionTreeSubjects, {
+              modelId: props.id,
+              groupId: "1305438642755218144"
+            })
+          onResult((queryResult)=> {
+            for(let subject of queryResult.data.permissionTreeSubjects.data){
+              if(subject.level == 7){
+                console.log(subject);
+                const { mutate } = useMutation(permissionRuleDelete, ()=>({
+                   variables:{
+                    "id": subject.permission_rule_id
+                   }
+                }))
+                const response_3 = mutate()
+                response_3
+                  .then(function(result){
+                    console.log(result);
+                  })
+              }
+            }
+            const { mutate } = useMutation(createRule, ()=>({
             variables:{
-                input: {
-                  model_type: "object",
-                  model_id: props.id,
-                  owner_type: "subject",
-                  owner_id: EXECUTORS.value[indexExecutor.value].id,
-                  level: 7
+              input: {
+                model_type: "object",
+                model_id: props.id,
+                owner_type: "subject",
+                owner_id: EXECUTORS.value[indexExecutor.value].id,
+                level: 7
                 }
               }
-          }))
-          console.log(1);
-          const response_2 = mutate()
-          response_2
-          .then(function (result){
-            $q.notify({
-              type: "positive",
-              message: "Модули обновлены",
-            });
+            }))
+            const response_2 = mutate()
+            response_2
+            .then(function (result){
+              console.log(result);
+              $q.notify({
+                type: "positive",
+                message: "Модули обновлены",
+              });
+            })
           })
         })
         .catch((err) => {
