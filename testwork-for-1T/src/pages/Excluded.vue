e20d0e693c30c449afffbd0b0971a77ae8b18bc4
Sheradab Urakov <sheradaburakov@gmail.com>
2023-04-13 20:30:50 +0300

    Добавленны таблицы в списки исключенных.

--- testwork-for-1T/src/pages/Excluded.vue
+++ testwork-for-1T/src/pages/Excluded.vue
@@ -1,3 +1,11 @@
 <template>
   <h5 class="flex flex-center">Список исключенных пуст</h5>
+  <table class="q-pa-md table">
+    <thead>
+      <th>Почта</th>
+      <th>Имя</th>
+      <th>Фамилия</th>
+      <th>Действия</th>
+    </thead>
+  </table>
 </template>
