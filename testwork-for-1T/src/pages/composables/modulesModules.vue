a13fff5f7bb22fa81b6576ff2cab34ae88b0cf77
Shidorix <danila.mazepa@gmail.com>
2023-04-15 19:20:23 +0300

    Сделал более улучшенную версию таблицы в Модулях

--- testwork-for-1T/src/pages/composables/modulesModules.vue
+++ testwork-for-1T/src/pages/composables/modulesModules.vue
@@ -1,89 +1,91 @@
 <template>
-  <div class="modules__table table">
-    <table class="modules__table-modules">
-      <thead>
-        <tr>
-          <th>Модуль</th>
-          <th>Ответственный</th>
-          <th>Дата начала</th>
-          <th>Дата окончания</th>
-          <th>Назначенные задачи</th>
-          <th>Выполненыне задачи</th>
-          <th>Завершеныне задачи</th>
-          <th>Действия</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="mod in MODULES" :key="mod.id">
-          <td>{{ mod.name }}</td>
-          <td>
-            {{
-              mod.property7?.fullname.first_name +
-              " " +
-              mod.property7?.fullname.last_name
-            }}
-          </td>
-          <td>{{ mod.property2?.date + " " + mod.property2?.time }}</td>
-          <td>{{ mod.property3?.date + " " + mod.property3?.time }}</td>
-          <td>
-            {{
-              mod.property8.reduce(function (a, b) {
-                if (b.property5 == "1700970386717883161") {
-                  return ++a;
-                } else return a;
-              }, 0)
-            }}
-          </td>
-          <td>
-            {{
-              mod.property8.reduce(function (a, b) {
-                if (b.property5 == "967659251654331262") {
-                  return ++a;
-                } else return a;
-              }, 0)
-            }}
-          </td>
-          <td>
-            {{
-              mod.property8.reduce(function (a, b) {
-                if (b.property5 == "1383309069201480491") {
-                  return ++a;
-                } else return a;
-              }, 0)
-            }}
-          </td>
-          <td>
-            <button
-              :disabled="disableRedBtn"
-              @click.self="
-                showForm_updateModule = !showForm_updateModule;
-                set_id($event, mod, task);
-              "
-              class="q-mr-sm btn"
-              :id="mod.id"
-            >
-              Редактировать
-            </button>
-            <button :disabled="disableRedBtn" class="btn" @click="deleteModule(mod)">Удалить</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <q-btn
-      :disabled="disableAddBtn"
-      class="q-mt-sm"
-      color="green"
-      @click="showForm_addModule = !showForm_addModule"
-      >Добавить модуль</q-btn
-    >
-    <q-dialog v-model="showForm_addModule">
+  
+  <q-table
+    :rows="MODULES"
+    :columns="columns"
+    :pagination="pagination"
+    :pagination-labels="{rowsPerPage: 'Строк на странице', rowsPerPageAll: 'Все'}"
+    :rows-per-page-options="[5, 10, 20]"
+  >
+    <template v-slot:body="props">
+      <q-tr :props="props">
+        <q-td>{{ props.row.name }}</q-td>
+        <q-td>
+          {{
+            props.row.property7?.fullname.first_name +
+            " " +
+            props.row.property7?.fullname.last_name
+          }}
+        </q-td>
+        <q-td>{{ props.row.property2?.date + " " + props.row.property2?.time }}</q-td>
+        <q-td>{{ props.row.property3?.date + " " + props.row.property3?.time }}</q-td>
+        <q-td>
+          {{
+            props.row.property8.reduce(function (a, b) {
+              if (b.property5 == "1700970386717883161") {
+                return ++a;
+              } else return a;
+            }, 0)
+          }}
+        </q-td>
+        <q-td>
+          {{
+            props.row.property8.reduce(function (a, b) {
+              if (b.property5 == "967659251654331262") {
+                return ++a;
+              } else return a;
+            }, 0)
+          }}
+        </q-td>
+        <q-td>
+          {{
+            props.row.property8.reduce(function (a, b) {
+              if (b.property5 == "1383309069201480491") {
+                return ++a;
+              } else return a;
+            }, 0)
+          }}
+        </q-td>
+        <q-td>
+          <q-btn
+            :disabled="disableRedBtn"
+            @click.self="
+              set_id($event, props.row, task);
+              
+            "
+            class="q-mr-sm btn"
+            :id="props.row.id"
+          >
+            Редактировать
+          </q-btn>
+          <q-btn :disabled="disableRedBtn" class="btn" @click="deleteModule(props.row)">Удалить</q-btn>
+        </q-td>
+      </q-tr>
+    </template >
+    
+    <template v-slot:top-right="props">
+      <q-btn
+        :disabled="disableAddBtn"
+        class="q-mt-sm"
+        color="green"
+        @click="showForm_addModule = !showForm_addModule"
+        >Добавить модуль</q-btn
+      >
+      
+    </template>
+
+    
+  </q-table>
+
+  <q-dialog v-model="showForm_addModule">
       <FormAddModule />
     </q-dialog>
     <q-dialog v-model="showForm_updateModule">
       <FormUpdateModule :mod="currentModuleClickUp" :idUpdateModule="id" />
     </q-dialog>
-  </div>
+
 </template>
+
 
 <script>
 import { computed, reactive, watch, ref } from "vue";
@@ -91,8 +93,7 @@ import { useStore } from "vuex";
 import { GetPropertyStatus } from "src/api/main/queryes";
 import { useQuery } from "@vue/apollo-composable";
 import FormAddModule from "components/FormAddModule.vue";
-import FormUpdateModule from "src/components/FormUpdateModule.vue";
-import { response } from "../../functions/functions";
+import FormUpdateModule from "src/components/formUpdateModule.vue";
 import { getClientOptions } from "src/apollo/index";
 import { provideApolloClient } from "@vue/apollo-composable";
 import { ApolloClient } from "@apollo/client/core";
@@ -119,7 +120,7 @@ export default {
     const idModule = ref(0);
     const store = useStore();
     const showForm_addModule = ref(false);
-    const showForm_updateModule = ref(false);
+    let showForm_updateModule = ref(false);
     const currentModuleClickUp = ref();
     const currentTaskClickUp = ref();
     store.dispatch("GET_MODULES");
@@ -127,6 +128,25 @@ export default {
     const module_index = computed(() => store.getters.MODULE_INDEX);
     const current_module = reactive({});
     const propertyStatus = reactive({});
+  
+    const pagination = reactive({
+      rowsPerPage: 10,
+      page: 1,
+      sortBy: 'name',
+    })
+
+
+  const columns = [
+  { name: 'Модуль', align: 'left', label: 'Модуль', field: 'Модуль', sortable: true },
+  { name: 'Ответственный', align: 'left', label: 'Ответственный', field: 'Ответственный', sortable: true },
+  { name: 'Дата начала', align: 'left', label: 'Дата начала', field: 'Дата начала' },
+  { name: 'Назначенные задачи', align: 'left', label: 'Назначенные задачи', field: 'Назначенные задачи' },
+  { name: 'Назначенные задачи', align: 'left', label: 'Назначенные задачи', field: 'Назначенные задачи' },
+  { name: 'Выполненные задачи', align: 'left', label: 'Выполненные задачи', field: 'Выполненные задачи', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+  { name: 'Действия', label: 'Действия', align: 'left', field: 'Действия', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+]
+
+
     const disableAddBtn = ref(false);
     const disableRedBtn = ref(false);
     const $q = useQuasar();
@@ -153,6 +173,8 @@ export default {
       }
     };
 
+    
+      
     const get_module = function (module_index) {
       current_module.values = MODULES.value[module_index.value];
     };
@@ -172,7 +194,6 @@ export default {
       current_module,
       deleteModule,
       propertyStatus,
-      showForm_updateModule,
       currentModuleClickUp,
       id,
       idModule,
@@ -182,11 +203,15 @@ export default {
       MODULES,
       disableAddBtn,
       disableRedBtn,
+      columns,
+      pagination,
       set_id(env, mod, task) {
         id.value = env.target.id;
         idUpdateModule.value = env.target.id;
         currentModuleClickUp.value = mod;
         currentTaskClickUp.value = task;
+        showForm_updateModule.value = !showForm_updateModule.value
+        
       },
     };
   },
