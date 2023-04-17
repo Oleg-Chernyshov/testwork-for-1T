4be6656ce5e507707a80deaf5ba8ff3072e935e8
Sheradab Urakov <sheradaburakov@gmail.com>
2023-04-17 23:10:21 +0300

    Улучшенное подключения к Stomp

--- testwork-for-1T/src/rabbitmq/client.js
+++
@@ -1,6 +0,0 @@
-import Stomp from "stompjs";
-
-const socket = new WebSocket("wss://stud.druid.1t.ru/ws");
-let client = Stomp.over(socket);
-
-export default client;
--- testwork-for-1T/src/rabbitmq/connect.js
+++ testwork-for-1T/src/rabbitmq/connect.js
@@ -2,11 +2,14 @@ import { provideApolloClient, useMutation } from '@vue/apollo-composable'
 import { ApolloClient } from '@apollo/client/core'
 import { createQueue } from 'src/api/main/mutations'
 import Cookies from 'js-cookie'
-import Client from 'src/rabbitmq/client'
 import { getClientOptions } from 'src/apollo/index'
+import Stomp from "stompjs";
 
 const apolloClient = new ApolloClient(getClientOptions())
 provideApolloClient(apolloClient)
+
+const socket = new WebSocket("wss://stud.druid.1t.ru/ws");
+let client = Stomp.over(socket);
 
 const { mutate: creatingQueue } = useMutation(createQueue)
 
@@ -29,19 +32,18 @@ const stompConnect = (store) => {
       setTimeout(() => {
         const refetch_modules = store.getters.REFETCH_MODULES
         refetch_modules()
-      const refetch_all_tasks = store.getters.REFETCH_ALL_TASKS
-      refetch_all_tasks()
-      const refetch_executors = store.getters.REFETCH_EXECUTORS
-      refetch_executors()
-      const refetch_responsibles = store.getters.REFETCH_RESPONSIBLES
-      refetch_responsibles()
+        const refetch_all_tasks = store.getters.REFETCH_ALL_TASKS
+        refetch_all_tasks()
+        const refetch_executors = store.getters.REFETCH_EXECUTORS
+        refetch_executors()
+        const refetch_responsibles = store.getters.REFETCH_RESPONSIBLES
+        refetch_responsibles()
       }, 500);
-      
-      
+
       message.ack();
     };
 
-    Client.subscribe(`/amq/queue/${queue}`, onMessage, { ack: 'client' })
+    client.subscribe(`/amq/queue/${queue}`, onMessage, { ack: 'client' })
   }
 
   const onError = (msg) => {
@@ -52,7 +54,7 @@ const stompConnect = (store) => {
     console.log('Close', msg)
   }
 
-  Client.connect('readonly', '@3P^Lgdab)sv', onConnect, onError, '/', onClose)
+  client.connect('readonly', '@3P^Lgdab)sv', onConnect, onError, '/', onClose)
 }
 
 const stompApi = { queueCreate, stompConnect }
